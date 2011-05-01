4ffbdb5ef0bb828f5637da71d481ee2241a131cf
okamuuu <okamuuu@gmail.com>
2011-05-01 12:32:05 +0900

    added lookup by key

--- lib/Gang/Groonga/Client.pm
+++ lib/Gang/Groonga/Client.pm
@@ -14,17 +14,66 @@ sub new {
     bless {%params}, $class; 
 }
 
-sub get_status { return $_[0]->_cmd("status"); }
+sub get_status {
+    my $self = shift;
 
-sub _cmd {
-    my ( $self, $cmd ) = @_;
-
-    my $res =
-      $self->ua->get("http://@{[ $self->host ]}:@{[ $self->port ]}/d/$cmd");
+    my $uri = $self->_base_uri("status");
+    my $res = $self->ua->get( $uri );
 
     Carp::croak("Groonga server has gone...") unless $res->is_success;
 
-    return JSON::decode_json($res->content);
+    return JSON::decode_json( $res->content );
+}
+
+sub lookup_by_key {
+    my ($self, $table, $key) = @_;
+
+    my $uri = $self->_base_uri("select");
+    $uri->query_form(table => $table, query => "_key:$key" );
+
+    warn $uri;
+
+    my $res = $self->ua->get($uri);
+    _croak_if_not_success($res); 
+
+    return JSON::decode_json( $res->content );
+}
+
+sub _croak_if_not_success {
+    my $res = shift;
+
+    if ( not $res->is_success ) {
+        Carp::croak(
+            "Groonga server return error status\n" . 
+            "status code: " . $res->code . "\n" .
+            "content: " . $res->content . "\n"
+        );
+    }
+
+    return $res;
+}
+
+sub _base_uri {
+    my ($self, $cmd) = @_;
+    
+    return URI->new("http://$self->{host}:$self->{port}/d/$cmd");
+}
+
+### stolen from FrePAN::FTS.pm
+sub make_query {
+    my ( $self, $query ) = @_;
+
+    my $ret = '';
+    for my $part (split /\s+/, $query) {
+        if ($part =~ s/^-//) {
+            $ret .= qq{ - "$part"} if $ret;
+        } else {
+            $ret .= qq{ + } if $ret;
+            $ret .= qq{"$part"};
+        }
+    }
+    warn "QUERY IS: $ret";
+    return $ret;
 }
 
 1;
--- t/groonga_client.t
+++ t/groonga_client.t
@@ -5,22 +5,29 @@ use t::TestUtils;
 use Test::More;
 
 BEGIN { use_ok 'Gang::Groonga::Client' }
-    
-my $grn = t::TestUtils->create_test_grn;
+   
+my ($grn, $client); 
+
+subtest 'prepare' => sub {
+    $grn = t::TestUtils->create_test_grn_with_10articles;
+    $client =
+      Gang::Groonga::Client->new( host => 'localhost', port => $grn->port );
+    isa_ok( $client, 'Gang::Groonga::Client' );
+};
 
 subtest 'request groonga server status' => sub {
-
-    my $client =
-      Gang::Groonga::Client->new( host => 'localhost', port => $grn->port );
-
-    isa_ok($client, 'Gang::Groonga::Client' );
-
     my $data = $client->get_status;
-
-    use Data::Dumper;
-    warn Dumper $data;
-
     ok ( $data->[1]->{"version"} ge "1.0.6" ), 'version check.';
 };
 
+subtest 'lookup article by _key' => sub {
+    my $data = $client->lookup_by_key('Article', 20110401000000);
+#    my $data = $client->lookup_
+    use Data::Dumper;
+    warn Dumper $data->[1]->[0]->[1];
+    warn Dumper $data->[1]->[0]->[2];
+    ok(1);
+};
+
 done_testing;
+
