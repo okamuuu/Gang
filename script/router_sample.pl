c963a2be15d789238835f2ceb6c27c87ae383074
okamuuu <okamuuu@gmail.com>
2011-05-22 12:11:38 +0900

    Router::Simpleのサンプルコード

---
+++ script/router_sample.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+use Router::Simple;
+use Data::Dumper;
+
+my $router = Router::Simple->new();
+$router->connect( '/blog/{year}/{month}',
+    { controller => 'Blog', action => 'monthly' } );
+
+my $env = {
+    'PATH_INFO'      => '/blog/2011/05',
+    'REQUEST_METHOD' => 'GET',
+};
+
+print Dumper my $matched_route = $router->match($env);
+
+=result
+$VAR1 = {
+          'controller' => 'Blog',
+          'month' => '05',
+          'action' => 'monthly',
+          'year' => '2011'
+        };
+=cut
+
+1;
+
