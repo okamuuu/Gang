51854c965c9de5cc1b1870832cdb2af50cdb6840
okamuuu <okamuuu@gmail.com>
2011-10-25 21:31:07 +0900

    added default-command-version param.

--- script/rebuild_grn_server.sh
+++ script/rebuild_grn_server.sh
@@ -2,5 +2,6 @@ mkdir -p groonga/db
 rm -fr groonga/db/*
 groonga -n --port 10041 groonga/db/test.db < groonga/schema.grn
 groonga --port 10041 groonga/db/test.db < groonga/data.grn
-groonga -s --port 10041 --protocol http groonga/db/test.db
+groonga -s --default-command-version 2 --port 10041 --protocol http groonga/db/test.db
+
 
