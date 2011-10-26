492fbab8cba128651cfb1dd120c35d22e8a07f79
okamuuu <okamuuu@gmail.com>
2011-10-26 22:45:17 +0900

    added some columns getter methods

--- lib/Gang/Model/Article.pm
+++ lib/Gang/Model/Article.pm
@@ -3,10 +3,28 @@ use strict;
 use warnings;
 use Class::Accessor::Lite 0.05 (
     new => 1,
-    ro  => [qw/id _key title keywords_ref content/]
+    ro  => [qw/id _key title keywords content display_fg published_at created_at updated_at/]
 );
 
-sub keywords { @{ $_[0]->{keywords_ref} } } 
+sub columns { return qw/_id _key title keywords content display_fg published_at created_at updated_at/ }
+
+sub list_columns { return qw/_id _key title keywords display_fg published_at created_at updated_at/ }
+
+sub type_of {
+    return (
+        _id          => 'UInt32',
+        _key         => 'ShortText',
+        title        => 'ShortText',
+        keywords     => 'ShortText',
+        content      => 'Text',
+        display_fg   => 'Bool',
+        published_at => 'Time',
+        created_at   => 'Time',
+        updated_at   => 'Time',
+    );
+}
+
+sub is_valid { return 1; }
 
 1;
 
